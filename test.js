eba0846454a8243a181cd35f5e1914ffb5e4d1d1
Alok Kumar <aloknh31@gmail.com>
2025-01-21 20:28:48 +0530

    tuples

--- test.js
+++ test.js
@@ -1,9 +1,2 @@
 "use strict";
-var a = 12;
-var b = 23;
-let c = 'a';
-let arr = [1, 2, 3, 4, "alok", { name: "alok" }];
-function addition(a, b) {
-    return a + b;
-}
-console.log(addition(a, b));
+let arr = ["alok", 6];
--- test.ts
+++ test.ts
@@ -1,8 +1,1 @@
-var a =12;
-var b = 23;
-let c ='a'
-let arr = [1,2,3,4,"alok" , {name:"alok"}] 
-function addition(a: number, b: number)  {
-    return a + b;
-}
-console.log(addition(a, b));
+let arr : [string,number] = ["alok", 6];
