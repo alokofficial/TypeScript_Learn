f0ef5da4dc275a0d3d7dd1dc6e26123a409c6f2a
Alok Kumar <aloknh31@gmail.com>
2025-01-21 20:24:55 +0530

    check the types

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 "use strict";
 var a = 12;
 var b = 23;
+let c = 'a';
+let arr = [1, 2, 3, 4, "alok", { name: "alok" }];
 function addition(a, b) {
     return a + b;
 }
--- test.ts
+++ test.ts
@@ -1,5 +1,7 @@
 var a =12;
 var b = 23;
+let c ='a'
+let arr = [1,2,3,4,"alok" , {name:"alok"}] 
 function addition(a: number, b: number)  {
     return a + b;
 }
