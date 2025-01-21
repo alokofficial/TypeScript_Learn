6a6a4435384091c17916d7ff2bf6e9ebe10afa29
Alok Kumar <aloknh31@gmail.com>
2025-01-21 20:08:56 +0530

    basic initiation of ts file

---
+++ test.js
@@ -0,0 +1,7 @@
+"use strict";
+var a = 12;
+var b = 23;
+function addition(a, b) {
+    return a + b;
+}
+console.log(addition(a, b));
---
+++ test.ts
@@ -0,0 +1,6 @@
+var a =12;
+var b = 23;
+function addition(a: number, b: number)  {
+    return a + b;
+}
+console.log(addition(a, b));
