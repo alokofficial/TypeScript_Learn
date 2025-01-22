6e142f5bf13a5d46145f92650224eeef05d7a517
Alok Kumar <aloknh31@gmail.com>
2025-01-22 12:26:20 +0530

    construction creation using direct method and useful

--- test.js
+++ test.js
@@ -9,4 +9,3 @@ class HumanMaker {
 let h1 = new HumanMaker("Alok", 23);
 let h2 = new HumanMaker("Sneha", 26);
 console.log(h1);
-console.log(h2);
--- test.ts
+++ test.ts
@@ -1,11 +1,7 @@
 // constructor
 
 class HumanMaker{
-    public name;
-    public age;
-    constructor(name: string,age: number){
-        this.name = name;
-        this.age=age;
+    constructor(public name: string, public age: number){
     }
 }
 
@@ -14,4 +10,3 @@ let h1= new HumanMaker("Alok", 23);
 let h2 = new HumanMaker("Sneha", 26);
 
 console.log(h1);
-console.log(h2);
