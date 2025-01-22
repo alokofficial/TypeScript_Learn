fe9997a02abf12358175635f57b961449ea6ddc6
Alok Kumar <aloknh31@gmail.com>
2025-01-22 10:57:12 +0530

    type aliasing

--- test.js
+++ test.js
@@ -1,25 +1,8 @@
 "use strict";
-// Defining interfaces
-function printUser(user) {
-    console.log(user.name);
-    console.log(user.email);
-    console.log(user.age);
+let a;
+let b;
+function abcd(obj) {
 }
-printUser({
-    name: "John",
-    email: "4p9bM@example.com",
-});
-function printAdmin(admin) {
-    admin.role;
-}
-printAdmin({
-    name: "John",
-    email: "4p9bM@example.com",
-    role: "admin"
-});
-function a(user) {
-    user.role,
-        user.email,
-        user.age,
-        user.name;
-}
+abcd(1);
+abcd("1");
+abcd(true);
--- test.ts
+++ test.ts
@@ -1,47 +1,14 @@
-// Defining interfaces
+// type aliases
+type sankhya = number;
+let a:sankhya;
 
-interface User {
-    name:string;
-    email:string;
-    age?:number;
-}
-function printUser(user:User){
-    console.log(user.name);
-    console.log(user.email);
-    console.log(user.age);
-}
-printUser({
-    name:"John",
-    email:"4p9bM@example.com",
-})
-// extending interfaces
-interface Admin extends User{
-    role:string;
+type value =string |number | boolean;
+let b:value;
+
+function abcd(obj:value){
+
 }
 
-function printAdmin(admin:Admin){
-    admin.role
-}
-printAdmin({
-    name:"John",
-    email:"4p9bM@example.com",
-    role:"admin"
-})
-
-// same name interfaces
-interface User1{
-    name:string;
-    email:string;
-    age?:number;
-}
-
-interface User1{
-    role:string;
-}  
-
-function a(user:User1){
-    user.role,
-    user.email,
-    user.age,
-    user.name
-}
+abcd(1);
+abcd("1");
+abcd(true);
