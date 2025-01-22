22b4fbc6607916842aa037f77533fd5910e2cb29
Alok Kumar <aloknh31@gmail.com>
2025-01-22 10:59:35 +0530

    difference between type aliases and interface

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
 "use strict";
-let a;
-let b;
-function abcd(obj) {
-}
-abcd(1);
-abcd("1");
-abcd(true);
+//interfaces can be extended but type is not
+//Type aliases are more flexible and versatile for defining various types, while interfaces are specifically designed for defining object-oriented contracts and structures. The choice between them depends on the specific use case and the desired level of flexibility and structure.
--- test.ts
+++ test.ts
@@ -1,14 +1,12 @@
-// type aliases
-type sankhya = number;
-let a:sankhya;
+type a=string;
+// type a=number; // can not merge
 
-type value =string |number | boolean;
-let b:value;
-
-function abcd(obj:value){
-
+interface b {
+    val1:string;
 }
+interface b{
+    val2:string;
+} // can merge
 
-abcd(1);
-abcd("1");
-abcd(true);
+//interfaces can be extended but type is not
+//Type aliases are more flexible and versatile for defining various types, while interfaces are specifically designed for defining object-oriented contracts and structures. The choice between them depends on the specific use case and the desired level of flexibility and structure.
